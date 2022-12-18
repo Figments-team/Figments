8837be9033ab1203eb05e8a290f8482e349be190
Tommaso De Tommaso <detommaso23@gmail.com>
2022-12-18 16:09:45 +0100

    Updated Figments

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -4,6 +4,6 @@ func self_direct():
 	await get_tree().create_timer(1).timeout
 	ProjectSettings.load_resource_pack("C:/Users/Tommaso/Documents/Synced Documents/Projects/Godot/Figments/Builds/James/James.zip")
 	await Overlay.fade_to_black()
-	await Director.Root.load_and_add_scene("res://Entry.tscn")
+	await Director.Root.load_and_add_scene(IO.get_scene_path("Entry", "James"))
 	await Overlay.fade_from_black()
 	queue_free()
